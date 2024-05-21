3840b4decaa016d1b68622f80cfb278d32b3d785
Sam K <skasimalla@gmail.com>
2024-05-20 22:34:04 -0400

    Updates

--- build-conan2.bat
+++ build-conan2.bat
@@ -10,7 +10,7 @@ echo "Create complete"
 conan version
 conan remote add conan-dev-local https://soleng.jfrog.io/artifactory/api/conan/conan-dev-local -f
 echo "Remote added "
-conan remote login conan-dev-local sam -p $ACCESS_TOKEN
+conan remote login conan-dev-local sam -p %ACCESS_TOKEN%
 
 REM conan list --graph=create.json --graph-binaries=build --format=json > pkglist.json
 REM conan upload --list=pkglist.json -r=conan-dev-local -c
